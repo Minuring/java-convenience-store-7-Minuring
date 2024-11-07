ad911810ab4e48dc0347bf937324298ad9ce6329
Minuring <mw9505@gmail.com>
2024-11-08 02:51:25 +0900

    feat(input): 추가 구매 여부 입력
    추가 구매 여부를 입력받습니다.

---
+++ src/test/java/store/presentation/view/input/BuyMoreInputViewTest.java
@@ -0,0 +1,20 @@
+package store.presentation.view.input;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static store.presentation.view.Message.INPUT_BUY_MORE;
+
+import org.junit.jupiter.api.Test;
+import store.presentation.view.IOTest;
+
+class BuyMoreInputViewTest extends IOTest {
+
+    @Test
+    void 지정한_입력요구_문구를_띄운다() throws Exception {
+        systemIn("Y");
+        BuyMoreInputView BuyMoreInputView = new BuyMoreInputView();
+
+        BuyMoreInputView.read();
+
+        assertThat(getOutput()).isEqualTo(INPUT_BUY_MORE.get() + System.lineSeparator());
+    }
+}
