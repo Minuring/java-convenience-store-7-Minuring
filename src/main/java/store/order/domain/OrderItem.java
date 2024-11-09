ea4a6f7fec131db07074f944078ceca07b4c4c15
Minuring <mw9505@gmail.com>
2024-11-09 16:31:56 +0900

    refactor(order): 주문 상품 클래스 수정
    계산 과정에서 Mutable하게 주고받도록 변경합니다.

--- src/main/java/store/order/domain/OrderItem.java
+++ src/main/java/store/order/domain/OrderItem.java
@@ -4,14 +4,27 @@ public class OrderItem {
 
     private final String name;
     private final int price;
-    private final int quantity;
-    private final int free;
+    private int quantity = 0;
+    private int free = 0;
+
+    public OrderItem(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
 
     public OrderItem(String name, int price, int quantity, int free) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
         this.free = free;
+    }
+
+    public void setFree(int free) {
+        this.free = free;
+    }
+
+    public void addQuantity(int quantity) {
+        this.quantity += quantity;
     }
 
     public int getFree() {
